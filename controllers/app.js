8be51c47a3d034cc3e1328ebfc7aa4f720c4e2bb
管世明 <guanshiming@aiztone.com>
2016-11-14 19:27:43 +0800

    添加package.json

--- controllers/app.js
+++ controllers/app.js
@@ -1,11 +1,17 @@
 /**
  * Created by lenn on 16/11/14.
  */
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send('GET request to the homepage')
 });
 
-var server = app.listen(8888, function () {});
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+})
+
+var server = app.listen(8888, function () {
+    console.log("server started at port 8888.")
+});
