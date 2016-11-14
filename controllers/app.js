f645663c75d11d77a9cab357d6f6a589eba68c56
管世明 <guanshiming@aiztone.com>
2016-11-14 19:44:17 +0800

    添加模版引擎

--- controllers/app.js
+++ controllers/app.js
@@ -4,12 +4,18 @@
 var express = require('express')
 var app = express()
 
+app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
     res.send('GET request to the homepage')
 });
 
 app.post('/', function (req, res) {
     res.send('POST request to the homepage')
+})
+
+app.get('/index', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 var server = app.listen(8888, function () {
